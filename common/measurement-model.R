4b90c0448b301bd37481de8e136e14b4dd41abe1
GCC <geiser@usp.br>
2017-10-25 22:34:13 -0200

    motivation instrument to measure skill

---
+++ common/measurement-model.R
@@ -0,0 +1,459 @@
+
+wants <- c('TAM', 'sirt', 'lavaan', 'ggrepel', 'parallel', 'reshape', 'ggplot2', 'dplyr', 'readr', 'readxl', 'multcomp')
+has   <- wants %in% rownames(installed.packages())
+if(any(!has)) install.packages(wants[!has])
+
+############################################################################
+## Functions to pre- and post- processing data                            ##
+############################################################################
+
+## Function to get data from motivation reports
+get_data_map_from_reports <- function(sources) {
+  library(readr)
+  library(dplyr)
+  
+  data_map <- mclapply(sources, FUN = function(x) {
+    dat <- read_excel(x$filename, sheet = x$sheed, col_types = "numeric")
+    rdat <- dat[x$wid]
+    for (endwith in x$end.withs) {
+      rdat <- merge(
+        rdat
+        , dplyr::select(dat, starts_with(x$wid), ends_with(endwith)))
+    }
+    for (inv_key in x$inv.keys) {
+      rdat[inv_key] <- 8-rdat[inv_key]
+    }
+    return(rdat)
+  }, mc.allow.recursive = FALSE)
+  
+  return(data_map)
+}
+
+############################################################################
+## Functions to get TAM models                                            ##
+############################################################################
+
+## Remove incorrect TAMs
+remove_incorrect_TAMs <- function(tam_models) {
+  tmp_tam_models <- tam_models
+  for(name in names(tmp_tam_models)) {
+    if (name != paste0(colnames(tmp_tam_models[[name]]$resp), collapse = '+')) {
+      cat('\n... removing TAM model with name: ', name, ' ...\n')
+      tam_models[[name]] <- NULL
+    }
+  }
+  return(tam_models)
+}
+
+## Function to get TAM from a name_model using dat
+get_TAM <- function(name_model, dat, irtmodel="GPCM") {
+  library(dplyr)
+  column_names <- strsplit(name_model,"[+]")[[1]]
+  dat_r <- dat[column_names] # columns for data.frame
+  return(tryCatch(tam.mml(dat_r, irtmodel=irtmodel), error = function(e) NULL)) # use tam to obtain the model
+}
+
+## Function to get TAMs models
+get_TAMs <- function(dat, column_names, tam_models = NULL, fixed = NULL
+                     , min_columns = 3, limit = 100, irtmodel = "GPCM") {
+  
+  library(TAM)
+  library(parallel)
+  
+  nro_generated_models <- 0
+  if (is.null(tam_models)) tam_models <- list()
+  column_names <- expand.grid(column_names)
+  
+  # add fixed columns
+  if (!is.null(fixed) & length(fixed) > 0) {
+    cnames <- colnames(column_names)
+    for (cname in fixed) {
+      column_names <- cbind(column_names, rep(cname, nrow(column_names))) 
+    }
+    colnames(column_names) <- c(cnames, fixed)
+  }
+  # get name_models
+  name_models <- do.call(paste, c(column_names , sep="+"))
+  name_models <- gsub("[+]NA", "", gsub("NA[+]","",name_models))
+  # generate tam models
+  nro_total_models <- 0
+  for (i in 1:length(name_models)) {
+    if (nchar(name_models[[i]]) < 2) next
+    if (nchar(name_models[[i]]) - nchar(gsub("[+]", "", name_models[[i]])) < min_columns-1) next
+    nro_total_models <- nro_total_models+1
+  }
+  
+  generated_name_models <- list()
+  for (i in 1:length(name_models)) {
+    if (nchar(name_models[[i]]) < 2) next
+    if (nchar(name_models[[i]]) - nchar(gsub("[+]", "", name_models[[i]])) < min_columns-1) next
+    if (any(names(tam_models) == name_models[i])) next
+    # use tam to obtain the model
+    generated_name_models[name_models[[i]]] <- name_models[[i]]
+    nro_generated_models <- nro_generated_models + 1
+    if (nro_generated_models >= limit) break
+  }
+  cat('\n... generating ', length(generated_name_models), ' of ', nro_total_models
+      , ' from ', length(tam_models), ' to ', length(tam_models)+length(generated_name_models), ' ...\n')
+  
+  if (nro_total_models == length(tam_models)+length(generated_name_models)) {
+    generated_tam_models <- lapply(generated_name_models, get_TAM, dat=dat, irtmodel = irtmodel)
+  } else {
+    generated_tam_models <- mclapply(generated_name_models, get_TAM, dat=dat, irtmodel = irtmodel, mc.allow.recursive = FALSE)
+  }
+  
+  for (name in names(generated_tam_models)) {
+    if (nchar(name) - nchar(gsub("[+]", "", name)) < min_columns-1) next
+    if (class(generated_tam_models[[name]]) != 'tam.mml') next
+    tam_models[[name]] <- generated_tam_models[[name]]
+  }
+  
+  tam_models <- remove_incorrect_TAMs(tam_models)
+  return(tam_models)
+}
+
+## Function to get mistfit information
+get_misfits_information <- function(tam_models) {
+  
+  library(TAM)
+  
+  tam_names <- c()
+  everything_fits <- c()
+  mis_infit_params <- c()
+  mis_outfit_params <- c()
+  
+  c_mis_infit_params <- c()
+  c_mis_outfit_params <- c()
+  
+  for (name in names(tam_models)) {
+    cat('\n...',name,'...\n')
+    
+    tam_fit <- tam.fit(tam_models[[name]])
+    if (is.null(tam_fit)) next
+    item_fit <- tam_fit$item
+    
+    misfits_in <- as.character(item_fit$parameter[!is.na(item_fit$Infit) & item_fit$Infit > 2])
+    misfits_out <- as.character(item_fit$parameter[!is.na(item_fit$Outfit) & item_fit$Outfit > 2])
+    
+    c_misfits_in <- length(misfits_in)
+    c_misfits_out <- length(misfits_out)
+    
+    misfits_in <- paste0(misfits_in, collapse = ',')
+    misfits_out <- paste0(misfits_out, collapse = ',')
+    
+    tam_names <- c(tam_names, name)
+    everything_fits <- c(everything_fits, (c_misfits_in == 0 && c_misfits_out == 0))
+    
+    mis_infit_params <- c(mis_infit_params, misfits_in)
+    mis_outfit_params <- c(mis_outfit_params, misfits_out)
+    
+    c_mis_infit_params <- c(c_mis_infit_params, c_misfits_in)
+    c_mis_outfit_params <- c(c_mis_outfit_params, c_misfits_out)
+  }
+  return(data.frame(
+    name = tam_names, everything.fits = everything_fits
+    , mis.infit.param = mis_infit_params, mis.outfit.param = mis_outfit_params
+    , c.mis.infit.param = c_mis_infit_params, c.mis.outfit.param = c_mis_outfit_params
+    ))
+}
+
+## Function to add information for TAM models
+add_info_for_TAM_models <- function(tam_models) {
+  
+  library(TAM)
+  
+  p <- c()
+  X2 <- c()
+  SRMR <- c()
+  name <- c()
+  p_holm <- c()
+  WLE.rel <- c()
+  EAP.rel <- c()
+  srmr_fit <- c()
+  
+  rel_fit <- c()
+  numcols <- c()
+  
+  model_fit <- c()
+  everything_fits <- c()
+  max_item_non_fits <- c()
+  
+  misfits_info <- get_misfits_information(tam_models)
+  
+  for (name_model in names(tam_models)) {
+    tam_mod <- tam_models[[name_model]]
+    if (is.null(tam_mod)) next;
+    
+    # model fit
+    fit_mod <- tam.modelfit(tam_mod)
+    srmr <- as.numeric(fit_mod$statlist$SRMR)
+    pholm <- as.numeric(fit_mod$modelfit.test$p.holm)
+    wle_val <- mean(tam.wle(tam_mod)$WLE.rel, na.rm = TRUE)
+    
+    # p indicates significative error of approximation (MADaQ3)
+    # p-holm indicates significative error of approximation (Maximum Chi square)
+    model_fit_v <- FALSE
+    if (pholm > 0.05 || as.numeric(fit_mod$stat.MADaQ3$p) > 0.05) {
+      model_fit_v <- TRUE
+    }
+    model_fit <- c(model_fit, model_fit_v)
+    
+    # SRMR less than 0.08 is good
+    srmr_v <- 'not good'
+    if (srmr < 0.08) {
+      srmr_v <- 'good'
+    }
+    if (srmr >= 0.08 && srmr < 0.1) {
+      srmr_v <- 'normal'
+    }
+    srmr_fit <- c(srmr_fit, srmr_v)
+    
+    # relability adequacy
+    rel_v <- FALSE
+    if (!is.null(wle_val) && !is.na(wle_val) &&
+        !is.null(tam_mod$EAP.rel) && !is.na(tam_mod$EAP.rel) &&
+        wle_val > 0.5 && tam_mod$EAP.rel > 0.5) {
+      rel_v <- TRUE
+    }
+    rel_fit <- c(rel_fit, rel_v)
+    
+    # model fits
+    everything_fits <- c(
+      everything_fits
+      , misfits_info$everything.fits[misfits_info$name == name_model])
+    max_item_non_fits <- c(max_item_non_fits, max(
+      misfits_info$c.mis.infit.param[misfits_info$name == name_model],
+      misfits_info$c.mis.outfit.param[misfits_info$name == name_model]))
+    
+    # add extra information
+    p <- c(p, as.numeric(fit_mod$stat.MADaQ3$p))
+    SRMR <- c(SRMR, srmr)
+    p_holm <- c(p_holm, pholm)
+    X2 <- c(X2, fit_mod$modelfit.test$maxX2)
+    name <- c(name, name_model)
+    WLE.rel <- c(WLE.rel, wle_val)
+    EAP.rel <- c(EAP.rel, tam_mod$EAP.rel)
+    
+    numcols <- c(numcols, length(strsplit(name_model, "[+]")[[1]]))
+  }
+  
+  return(list(
+    misfits.info = misfits_info,
+    information = data.frame(
+      name = name, numcols = numcols, model_fit = model_fit, srmr_fit = srmr_fit
+      , everything.fits = everything_fits, max.item.non.fits  = max_item_non_fits
+      , X2 = X2, p_holm = p_holm, p = p, SRMR = SRMR, rel_fit = rel_fit, WLE.rel = WLE.rel, EAP.rel = EAP.rel)))
+}
+
+
+## Function to evaluate the unidimensionality lavaan
+test_lav <- function(tam_mod) {
+  
+  library(lavaan)
+  
+  cat('\n... test by lavaan: ', paste0(colnames(tam_mod$resp), collapse = '+'),' ...\n')
+  
+  # linear CFA with lavaan
+  lavmodel <- paste0("\n Ability =~ ", paste(colnames(tam_mod$resp), collapse = "+"), "\n Ability ~~ 1*Ability \n")
+  lav_mod <- tryCatch(lavaan::sem(lavmodel , data = tam_mod$resp , missing="fiml", std.lv=TRUE), error = function(e) NULL)
+  
+  fail <- TRUE
+  lav_cfi_val <- NA
+  lav_tli_val <- NA
+  lav_rmsea_pvalue_val <- NA
+  
+  if (!is.null(lav_mod) && !is.null(tryCatch(fitMeasures(lav_mod), error = function(e) NULL))) {
+    fail <- FALSE
+    lav_cfi_val <- as.numeric(fitMeasures(lav_mod, "cfi"))
+    lav_tli_val <- as.numeric(fitMeasures(lav_mod, "tli"))
+    lav_rmsea_pvalue_val <- as.numeric(fitMeasures(lav_mod, "rmsea.pvalue"))
+    if (!is.na(lav_rmsea_pvalue_val) && lav_rmsea_pvalue_val < 0.05) {
+      fail <- TRUE
+    }
+  }
+  
+  return(list(fail = fail, lav_mod = lav_mod
+              , lav_cfi_val = lav_cfi_val, lav_tli_val = lav_tli_val
+              , lav_rmsea_pvalue_val = lav_rmsea_pvalue_val))
+}
+
+## test unidimensionality by residual pca
+test_uni_by_pca <- function(tam_mod) {
+  library(TAM)
+  library(psych)
+  
+  res <- IRT.residuals(tam_mod)$residuals
+  
+  pca_mod <- pca(res, nfactors = ncol(res))
+  fail = !all(colSums(abs(as.data.frame(unclass(pca_mod$loadings))) > 0.4) == 1)
+  
+  return(list(fail = fail))
+}
+
+## Function to filter non-unidimensional vs unidimensional TAMs
+add_unidimensionality_test_info_for_TAM_models <- function(tam_models, information = NULL, itemequals = NULL) {
+  
+  resp_detect_tests <- lapply(tam_models, test_detect, itemequals = itemequals)
+  resp_test_uni_by_pca <- lapply(tam_models, test_uni_by_pca)
+  # detect filtering
+  unidim_test <- c()
+  DETECT <- c()
+  DETECT_VAL <- c()
+  DETECT_ASSI <- c()
+  DETECT_RATIO <- c()
+  test_uni_by_res_pca <- c()
+  for (name in names(tam_models)) {
+    resp_detect_test <- resp_detect_tests[[name]]
+    unidim_test <- c(unidim_test, !resp_detect_test$fail)
+    DETECT <- c(DETECT, resp_detect_test$detect)
+    DETECT_VAL <- c(DETECT_VAL, resp_detect_test$detect_val)
+    DETECT_ASSI <- c(DETECT_ASSI, resp_detect_test$detect_assi)
+    DETECT_RATIO <- c(DETECT_RATIO, resp_detect_test$detect_ratio)
+    test_uni_by_res_pca <- c(test_uni_by_res_pca, !resp_test_uni_by_pca[[name]]$fail)
+  }
+  
+  detect.infomation <- data.frame(
+    name = names(tam_models)
+    , unidim_test = unidim_test
+    , DETECT = DETECT, DETECT_VAL = DETECT_VAL, DETECT_ASSI = DETECT_ASSI, DETECT_RATIO
+    , uni.by.pca = test_uni_by_res_pca)
+  
+  if (!is.null(information)) {
+    detect.infomation <- merge(information, detect.infomation, by = 'name', all.x = TRUE)
+  }
+  
+  resp_lav_tests <- lapply(tam_models, test_lav)
+  
+  # lavaan filtering
+  unidim_lav_test <- c()
+  lav_CFI <- c()
+  lav_TLI <- c()
+  lav_rmsea_pvalue <-c()
+  for (name in names(tam_models)) {
+    resp_lav_test <- resp_lav_tests[[name]]
+    if (!is.null(resp_lav_test)) {
+      unidim_lav_test <- c(unidim_lav_test, !resp_lav_test$fail)
+      lav_CFI <- c(lav_CFI, resp_lav_test$lav_cfi_val)
+      lav_TLI <- c(lav_TLI, resp_lav_test$lav_tli_val)
+      lav_rmsea_pvalue <- c(lav_rmsea_pvalue, resp_lav_test$lav_rmsea_pvalue_val)
+    } else {
+      unidim_lav_test <- c(unidim_lav_test, FALSE)
+      lav_CFI <- c(lav_CFI, NA)
+      lav_TLI <- c(lav_TLI, NA)
+      lav_rmsea_pvalue <- c(lav_rmsea_pvalue, NA)
+    }
+  }
+  
+  lav.infomation <- data.frame(
+    name = names(tam_models)
+    , unidim_lav_test = unidim_lav_test
+    , lav_CFI = lav_CFI, lav_TLI = lav_TLI, lav_rmsea_pvalue = lav_rmsea_pvalue)
+  
+  full.infomation <- merge(detect.infomation, lav.infomation, by = 'name', all.x = TRUE)
+  
+  ###
+  list_nonunidimensional_models <- c()
+  list_unidimensional_models <- c()
+  for (name in names(tam_models)) {
+    if (resp_detect_tests[[name]]$fail || resp_lav_tests[[name]]$fail) {
+      list_nonunidimensional_models <- c(list_nonunidimensional_models, name)
+    } else {
+      list_unidimensional_models <- c(list_unidimensional_models, name)
+    }
+  }
+  
+  return(list(
+    information = full.infomation
+    , full.unidimensional_models = list_unidimensional_models
+    , non.full.unidimensional_models = list_nonunidimensional_models
+  )
+  )
+}
+
+## Function to evaluate the unidimensionality by DETECT
+test_detect <- function(tam_mod, itemequals = NULL) {
+  library(sirt)
+  
+  # polydetect statistic analysis
+  cat('\n... test by detect: ', paste0(colnames(tam_mod$resp), collapse = '+'), ' ...\n')
+  
+  wle_mod <- tam.wle(tam_mod)
+  itemcluster <- colnames(tam_mod$resp)
+  if (!is.null(itemequals)) {
+    for (n_col in names(itemequals)) {
+      itemcluster[itemcluster %in% itemequals[[n_col]]]  <- n_col
+    }
+  }
+  
+  # all items are cluster for only one lattent factor
+  detect_mod <- conf.detect(data = tam_mod$resp, score = wle_mod$theta, itemcluster = itemcluster)
+  
+  # evaluate detect value
+  detect <- NA
+  detect_val <- max(abs(detect_mod$detect.summary['DETECT',]), na.rm = T)
+  detect_assi <- max(abs(detect_mod$detect.summary['ASSI',]), na.rm = T)
+  detect_ratio <- max(abs(detect_mod$detect.summary['RATIO',]), na.rm = T)
+  
+  fail <- FALSE
+  if (!is.na(detect_val) && detect_val >= 1) {
+    fail <- TRUE
+    detect <- "Strong multi"
+  }
+  if (!is.na(detect_val) && detect_val >= 0.4 && detect_val < 1) {
+    detect <- "Moderate multi"
+  }
+  if (!is.na(detect_val) && detect_val >= 0.2 && detect_val < 0.4) {
+    detect <- "Weak multi"
+  }
+  if (!is.na(detect_val) && detect_val < 0.2) {
+    detect <- "Unidimensional"
+    if (!is.na(detect_assi) && !is.na(detect_ratio) && detect_assi < 0.25 && detect_ratio < 0.36) {
+      detect <- "Essential Unidimensional"
+    }
+  }
+  
+  return(list(
+    fail = fail, detect_mod = detect_mod, detect = detect, detect_val = detect_val
+    , detect_assi = detect_assi, detect_ratio = detect_ratio))
+}
+
+
+## load and save TAMs to measure change
+load_and_save_TAMs_to_measure_skill <- function(dat, column_names, prefix, fixed = NULL, url_str = NULL, min_columns = 3, itemequals = NULL, irtmodel = "GPCM") {
+  
+  info_tam_models <- NULL
+  file_tam_info_str <- paste0(prefix, '_info_tam_models.RData')
+  if (file.exists(file_tam_info_str)) {
+    info_tam_models <- get(load(file_tam_info_str))
+  } else {
+    
+    # get tam_models
+    tam_models <- list()
+    file_tam_models_str <- paste0(prefix, '_tam_models.RData')
+    if (file.exists(file_tam_models_str)) {
+      tam_models <- get(load(file_tam_models_str))
+    } else if (!is.null(url_str)) {
+      url_con <- url(url_str)
+      tam_models <- tryCatch(get(load(url_con)), error = function(e) list())
+      close(url_con)
+    }
+    tam_models <- remove_incorrect_TAMs(tam_models)
+    
+    # get TAMs information
+    repeat {
+      curr_length <- length(tam_models)
+      tam_models <- get_TAMs(
+        dat, column_names = column_names, tam_models = tam_models, fixed = fixed
+        , min_columns = min_columns, limit = 75, irtmodel = irtmodel)
+      if (curr_length >= length(tam_models)) break
+      save(tam_models, file = file_tam_models_str)
+    }
+    info_tam_models <- add_info_for_TAM_models(tam_models)
+    info_tam_models <- add_unidimensionality_test_info_for_TAM_models(
+      tam_models, information = info_tam_models$information, itemequals = itemequals)
+    save(info_tam_models, file = file_tam_info_str)
+  }
+  
+  return(info_tam_models)
+}
+
---
+++ exp2/case03/07-motivation-measurment-instrument-generation.R
@@ -0,0 +1,189 @@
+library(TAM)
+
+library(readxl)
+
+library(parallel)
+#options(mc.cores=7)
+
+sources = list(
+  "Interest/Enjoyment" = list(
+    sheed = "data", wid = "UserID", name = "Interest/Enjoyment"
+    , filename = "report/motivation/interest-enjoyment/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("IE")
+    , inv.keys = c()
+    , column_names = list(Item08IE = c(NA, "Item08IE"), Item09IE = c(NA, "Item09IE")
+                          , Item11IE = c(NA, "Item11IE"), Item12IE = c(NA, "Item12IE")
+                          , Item21IE = c(NA, "Item21IE"), Item24IE = c(NA, "Item24IE"))
+    , prefix =  "case03_interest_enjoyment", min_columns = 4
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = NULL
+  )
+  , "Perceived Choice" = list(
+    sheed = "data", wid = "UserID", name = "Perceived Choice"
+    , filename = "report/motivation/perceived-choice/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("PC")
+    , inv.keys = c()
+    , column_names = list(Item03PC = c(NA, "Item03PC"), Item05PC = c(NA, "Item05PC")
+                          , Item13PC = c(NA, "Item13PC"), Item18PC = c(NA, "Item18PC")
+                          , Item20PC = c(NA, "Item20PC"))
+    , prefix =  "case03_perceived_choice", min_columns = 3
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = NULL
+  )
+  , "Pressure/Tension" = list(
+    sheed = "data", wid = "UserID", name = "Pressure/Tension"
+    , filename = "report/motivation/pressure-tension/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("PT")
+    , inv.keys = c("Item23PT")
+    , column_names = list(Item02PT = c(NA, "Item02PT"), Item04PT = c(NA, "Item04PT")
+                          , Item06PT = c(NA, "Item06PT"), Item23PT = c(NA, "Item23PT"))
+    , prefix =  "case03_pressure_tension", min_columns = 2
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = NULL
+  )
+  , "Effort/Importance" = list(
+    sheed = "data", wid = "UserID", name = "Effort/Importance"
+    , filename = "report/motivation/effort-importance/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("EI")
+    , inv.keys = c("Item01EI", "Item19EI")
+    , column_names = list(Item01EI = c(NA, "Item01EI"), Item14EI = c(NA, "Item14EI")
+                          , Item19EI = c(NA, "Item19EI"), Item22EI = c(NA, "Item22EI"))
+    , prefix =  "case03_effort_importance", min_columns = 2
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = NULL
+  )
+  , "Intrinsic Motivation" = list(
+    sheed = "data", wid = "UserID", name = "Intrinsic Motivation"
+    , filename = "report/motivation/intrinsic-motivation/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("IE","PC","PT","EI")
+    , inv.keys = c("Item05PC", "Item20PC", "Item18PC", "Item13PC", "Item03PC"
+                   , "Item02PT",  "Item06PT", "Item04PT", "Item01EI",  "Item19EI")
+    , column_names = list(Item08IE = c(NA, "Item08IE"), Item09IE = c(NA, "Item09IE")
+                          , Item11IE = c(NA, "Item11IE"), Item12IE = c(NA, "Item12IE")
+                          , Item21IE = c(NA, "Item21IE"), Item24IE = c(NA, "Item24IE")
+                          
+                          , Item03PC = c(NA, "Item03PC"), Item05PC = c(NA, "Item05PC")
+                          , Item13PC = c(NA, "Item13PC"), Item18PC = c(NA, "Item18PC")
+                          , Item20PC = c(NA, "Item20PC")
+                          
+                          , Item02PT = c(NA, "Item02PT"), Item04PT = c(NA, "Item04PT")
+                          , Item06PT = c(NA, "Item06PT"), Item23PT = c(NA, "Item23PT")
+                          
+                          , Item01EI = c(NA, "Item01EI"), Item14EI = c(NA, "Item14EI")
+                          , Item19EI = c(NA, "Item19EI"), Item22EI = c(NA, "Item22EI"))
+    , prefix =  "case03_intrinsic_motivation", min_columns = 16
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = list(ItemIE=c('Item08IE', 'Item09IE', 'Item11IE', 'Item12IE', 'Item21IE', 'Item24IE')
+                        , ItemPC=c('Item03PC','Item05PC', 'Item13PC', 'Item18PC', 'Item20PC')
+                        , ItemPT=c('Item02PT', 'Item04PT', 'Item06PT', 'Item23PT')
+                        , ItemEI=c('Item01EI', 'Item14EI', 'Item19EI', 'Item22EI'))
+  )
+  , "Attention" = list(
+    sheed = "data", wid = "UserID", name = "Attention"
+    , filename = "report/motivation/attention/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("A")
+    , inv.keys = c()
+    , column_names = list(Item18A = c(NA, "Item18A"), Item20A = c(NA, "Item20A")
+                          , Item25A = c(NA, "Item25A"), Item07A = c(NA, "Item07A")
+                          , Item13A = c(NA, "Item13A"))
+    , prefix =  "case03_attention", min_columns = 3
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = NULL
+  )
+  , "Satisfaction" = list(
+    sheed = "data", wid = "UserID", name = "Satisfaction"
+    , filename = "report/motivation/satisfaction/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("S")
+    , inv.keys = c("Item21S")
+    , column_names = list(Item01S = c(NA, "Item01S"), Item02S = c(NA, "Item02S")
+                          , Item05S = c(NA, "Item05S"), Item21S = c(NA, "Item21S"))
+    , prefix =  "case03_satisfaction", min_columns = 2
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = NULL
+  )
+  , "Level of Motivation" = list(
+    sheed = "data", wid = "UserID", name = "Level of Motivation"
+    , filename = "report/motivation/level-of-motivation/AnovaAnalysis.xlsx"
+    , start.with = c("Item"), end.withs = c("A", "S")
+    , inv.keys = c("Item21S")
+    , column_names = list(Item18A = c(NA, "Item18A"), Item20A = c(NA, "Item20A")
+                          , Item25A = c(NA, "Item25A"), Item07A = c(NA, "Item07A")
+                          , Item13A = c(NA, "Item13A")
+                          
+                          , Item01S = c(NA, "Item01S"), Item02S = c(NA, "Item02S")
+                          , Item05S = c(NA, "Item05S"), Item21S = c(NA, "Item21S"))
+    , prefix =  "case03_level_of_motivation", min_columns = 7
+    , fixed = NULL
+    , url_str = NULL
+    , itemequals = list(ItemA=c('Item18A', 'Item20A', 'Item25A', 'Item07A', 'Item13A')
+                        , ItemS=c('Item01S','Item02S', 'Item05S', 'Item21S'))
+  )
+)
+
+data_map <- get_data_map_from_reports(sources)
+
+tam_info_models_map <- lapply(sources, FUN = function(x, data_map) {
+  tam_info_models <- load_and_save_TAMs_to_measure_skill(
+    data_map[[x$name]]
+    , column_names = x$column_names
+    , url_str = x$url_str
+    , itemequals = x$itemequals
+    , prefix = x$prefix, min_columns = x$min_columns
+    , irtmodel = "RSM")
+  return(tam_info_models)
+}, data_map = data_map)
+
+
+
+
+
+
+
+View(tam_info_models$information)
+
+
+
+
+
+info_for_TAM_models <- add_info_for_TAM_models(tam_models)
+info_for_TAM_models <- add_unidimensionality_test_info_for_TAM_models(
+  tam_models, information = info_for_TAM_models$information
+  , itemequals = list(ItemIE=c('Item08IE', 'Item09IE','Item11IE', 'Item12IE', 'Item21IE', 'Item24IE')))
+
+
+
+res <- IRT.residuals(tam_models$`Item08IE+Item09IE+Item11IE+Item21IE+Item24IE`)$residuals
+
+expl.detect(tam_models$`Item08IE+Item09IE+Item11IE+Item21IE+Item24IE`)
+
+
+
+all(abs(pca_mod$loadings) > 0.3)
+rownames(pca_mod$loadings)[abs(pca_mod$loadings) < 0.35]
+
+
+
+
+
+sum(abs(as.data.frame(unclass(pca_mod$loadings))) > 0.3)
+
+max(pca_mod$loadings)
+
+resp_filter_TAMs <- filter_by_test_unidimensionality(
+  tam_models = tam_models, information = resp_TAMs$information, itemequals = itemequals)
+
+
+
+misfits_information <- get_misfits_information(tam_models)
+
+save(generated_tam_mods, file="generated_tam_mods.RData")
+
+View(tam.fit(mod)$itemfit)
+
