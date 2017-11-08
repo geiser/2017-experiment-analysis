e4365465cf32cd5b695bea4d84be1a0e27b654d9
Geiser gcc <geiser.gcc@gmail.com>
2017-11-08 22:17:13 +0000

    prepos info tam

--- exp2/case03/08-learning-outcome-measurement-building.R
+++ exp2/case03/08-learning-outcome-measurement-building.R
@@ -7,8 +7,6 @@ library(parallel)
 preData <- read_csv("data/SourcePreTest.csv")
 posData <- read_csv("data/SourcePosTest.csv")
 
-## get pre and post tams
-
 sdat <- get_stacking_data(
   preData, posData, wid = "UserID",
   items.pre = c("Re2","Un2","Ap1","Ap3","An3a","An3b","Ev2"),
@@ -16,24 +14,7 @@ sdat <- get_stacking_data(
   same.items = list(pre=c("Re2","An3a","An3b","Ev2"),
                     pos=c("ReB","AnC1","AnC2","EvB")))
 
-tam_info_mod <- load_and_save_TAMs_to_measure_skill(
-  sdat$resp
-  , column_names = list(Re2ReB=c(NA,"Re2ReB"),
-                        Un2=c(NA,"Un2"), UnB=c(NA,"UnB"),
-                        Ap1=c(NA,"Ap1"), ApA=c(NA,"ApA"),
-                        Ap3=c(NA,"Ap3"), ApC=c(NA,"ApC")
-                        , An3aAnC1=c(NA,"An3aAnC1")
-                        , An3bAnC2=c(NA,"An3bAnC2")
-                        , Ev2EvB=c(NA,"Ev2EvB")
-                        )
-  #, url_str = x$url_str
-  , itemequals = list(Re=c("Re2ReB"), Un=c("Un2","UnB"), Ap=c("Ap1","ApA","Ap3","ApC")
-                      , An=c("An3aAnC1", "An3bAnC2"), Ev=c("Ev2EvB"))
-  , prefix = "case03_prepos"
-  , fixed = NULL #c("Re2ReB","An3aAnC1","An3bAnC2","Ev2EvB")
-  , fixed_sets = list(s1=c("Un2","UnB"), s2=c("Ap1","ApA"), s3=c("Ap3","ApC"))
-  , min_columns = 4, irtmodel = "2PL")
-View(tam_info_mod$information)
+## get pre and post tams
 
 pre_tam_info_mod <- load_and_save_TAMs_to_measure_skill(
   preData[c("Re2","Un2","Ap1","Ap3","An3a","An3b","Ev2")]
@@ -54,8 +35,7 @@ pos_tam_info_mod <- load_and_save_TAMs_to_measure_skill(
   , column_names = list(ReB=c(NA,"ReB"), UnB=c(NA,"UnB")
                         , ApA=c(NA,"ApA"), ApC=c(NA,"ApC")
                         , AnC1=c(NA,"AnC1"), AnC2=c(NA,"AnC2")
-                        , EvB=c(NA,"EvB")
-  )
+                        , EvB=c(NA,"EvB"))
   #, url_str = x$url_str
   , itemequals = list(Re=c("ReB"), Un=c("UnB"), Ap=c("ApA","ApC")
                       , An=c("AnC1", "AnC2"), Ev=c("EvB"))
@@ -68,13 +48,18 @@ View(pos_tam_info_mod$information)
 #View(tam_info_mod$information)
 #View(pre_tam_info_mod$information)
 
-mod <- TAM.measure_change(
+if (file.exists('case03_prepos_mod.RData')) {
+  mod <- get(load('case03_prepos_mod.RData'))
+} else {
+  mod <- TAM.measure_change(
     preData, posData
-    , items.pre = c("Re2","Un2","Ap1","Ap3","An3a","An3b","Ev2")
+    , items.pre = c("Un2","Ap1","Ap3","An3a","An3b","Ev2")
     , items.pos = c("ReB","UnB","ApA","ApC","AnC1","AnC2","EvB")
-    , same_items.pre = c("Re2","An3a","An3b","Ev2")
-    , same_items.pos = c("ReB","AnC1","AnC2","EvB")
+    , same_items.pre = c("An3a","An3b","Ev2")
+    , same_items.pos = c("AnC1","AnC2","EvB")
     , userid = "UserID", verify = T, plotting = F, remove_outlier = T, irtmodel = "2PL")
+  save(mod, file = 'case03_prepos_mod.RData')
+}
 
 write_measure_change_report(
   mod, 'report/learning-outcome/', 'MeasurementChangeModel.xlsx', override = T
@@ -83,4 +68,4 @@ write_measure_change_report(
 write_change_measurement_model_plots(
   mod, 'report/learning-outcome/measurement-change-model-plots/', override = T
 )
-plot(mod$info.stacking$pre_mod)
+
