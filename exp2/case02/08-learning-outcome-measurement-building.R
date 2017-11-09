c240b6cf8e04572e3a340ea6aa57792a4ef9b8d3
Geiser gcc <geiser.gcc@gmail.com>
2017-11-09 01:32:32 +0000

    update case02 prepos information

--- exp2/case02/08-learning-outcome-measurement-building.R
+++ exp2/case02/08-learning-outcome-measurement-building.R
@@ -23,37 +23,42 @@ pre_tam_info_mod <- load_and_save_TAMs_to_measure_skill(
                         , An3a=c(NA,"An3a"), An3b=c(NA,"An3b")
                         , Ev2=c(NA,"Ev2"))
   #, url_str = url_str
-  , itemequals = list(Re=c("Re2"), Un=c("Un2"), Ap=c("Ap1","Ap3")
+  , itemequals = list(Re=c("Re1"), Un=c("Un3"), Ap=c("Ap2a","Ap2b")
                       , An=c("An3a", "An3b"), Ev=c("Ev2"))
   , prefix = "case02_pre"
-  , fixed = NULL #c("Re2ReB","An3aAnC1","An3bAnC2","Ev2EvB")
+  , fixed = NULL
   , min_columns = 4, irtmodel = "2PL")
 View(pre_tam_info_mod$information)
 
 pos_tam_info_mod <- load_and_save_TAMs_to_measure_skill(
-  posData[c("ReB","UnB","ApA","ApC","AnC1","AnC2","EvB")]
-  , column_names = list(ReB=c(NA,"ReB"), UnB=c(NA,"UnB")
-                        , ApA=c(NA,"ApA"), ApC=c(NA,"ApC")
+  posData[c("ReA","UnC","ApB1","ApB2","AnC1","AnC2","EvB")]
+  , column_names = list(ReA=c(NA,"ReA"), UnC=c(NA,"UnC")
+                        , ApB1=c(NA,"ApB1"), ApB2=c(NA,"ApB2")
                         , AnC1=c(NA,"AnC1"), AnC2=c(NA,"AnC2")
                         , EvB=c(NA,"EvB"))
   #, url_str = x$url_str
-  , itemequals = list(Re=c("ReB"), Un=c("UnB"), Ap=c("ApA","ApC")
+  , itemequals = list(Re=c("ReA"), Un=c("UnC"), Ap=c("ApB1","ApB2")
                       , An=c("AnC1", "AnC2"), Ev=c("EvB"))
   , prefix = "case02_pos"
-  , fixed = NULL #c("Re2ReB","An3aAnC1","An3bAnC2","Ev2EvB")
+  , fixed = NULL
   , min_columns = 4, irtmodel = "2PL")
 View(pos_tam_info_mod$information)
 
 #View(tam_info_mod$information)
 #View(pre_tam_info_mod$information)
 
-mod <- GPCM.measure_change(
-  preData, posData
-  , items.pre = c("Re1","Un3","Ap2a","Ap2b","An3a","An3b","Ev2")
-  , items.pos = c("ReA","UnC","ApB1","ApB2","AnC1","AnC2","EvB")
-  , same_items.pre = c("Ap2a","Ap2b","Ev2")
-  , same_items.pos = c("ApB1","ApB2","EvB")
-  , userid = "UserID", verify = T, plotting = T, remove_outlier = T, tam_models = NULL)
+if (file.exists('case02_prepos_mod.RData')) {
+  mod <- get(load('case02_prepos_mod.RData'))
+} else {
+  mod <- TAM.measure_change(
+    preData, posData
+    , items.pre = c("Un3","Ap2a","Ap2b","An3a","An3b")
+    , items.pos = c("UnC","ApB1","ApB2","AnC1","AnC2")
+    , same_items.pre = c("Ap2a","Ap2b")
+    , same_items.pos = c("ApB1","ApB2")
+    , userid = "UserID", verify = T, plotting = F, remove_outlier = T, irtmodel = "2PL")
+  save(mod, file = 'case02_prepos_mod.RData')
+}
 
 write_measure_change_report(
   mod, 'report/learning-outcome/', 'MeasurementChangeModel.xlsx', override = F
