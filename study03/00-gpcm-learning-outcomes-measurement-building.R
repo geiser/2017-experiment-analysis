997bc7355469496f57912e48372ce271442792d3
Geiser <geiser@usp.br>
2018-06-14 13:19:22 -0300

    update simulation for building measurment instrument of learning outcomes

--- study03/00-gpcm-learning-outcomes-measurement-building.R
+++ study03/00-gpcm-learning-outcomes-measurement-building.R
@@ -36,8 +36,11 @@ t(pre_info[pre_info$model_fit
            & pre_info$unidim_lav_test
            & pre_info$lav_CFI > 0.9
            ,])
-pre_mdl_strs <- c("Un2+Ap1+Ap3+An3a+An3b+Ev2"
-                  ,"Ap1+Ap3+An3a+An3b+Ev2","Re2+Ap1+Ap3+An3b+Ev2","Un2+Ap1+Ap3+An3b+Ev2")
+pre_mdl_strs <- list()
+pre_mdl_strs[[1]] <- c("Un2","Ap1","Ap3","An3a","An3b","Ev2")
+pre_mdl_strs[[2]] <- c("Ap1","Ap3","An3a","An3b","Ev2")
+pre_mdl_strs[[3]] <- c("Re2","Ap1","Ap3","An3b","Ev2")
+pre_mdl_strs[[4]] <- c("Un2","Ap1","Ap3","An3b","Ev2")
 
 ##
 pos_tam_info_models <- load_and_save_TAMs_to_measure_skill(
@@ -62,66 +65,71 @@ t(pos_info[pos_info$model_fit
            & pos_info$unidim_lav_test
            & pos_info$lav_CFI > 0.9
            ,])
-pos_mdl_strs <- c("ReB+UnB+ApA+ApC+AnC2+EvB+PFs3+PGs2"
-                  , "ReB+ApA+ApC+AnC1+AnC2+EvB+PGs3", "ReB+ApA+ApC+AnC2+EvB+PFs1+PGs1", "ReB+UnB+ApA+ApC+AnC2+EvB+PGs3")
+pos_mdl_strs <- list()
+pos_mdl_strs[[1]] <- c("ReB","UnB","ApA","ApC","AnC2","EvB","PFs3","PGs2")
+pos_mdl_strs[[2]] <- c("ReB","ApA","ApC","AnC1","AnC2","EvB","PGs3")
+pos_mdl_strs[[3]] <- c("ReB","ApA","ApC","AnC2","EvB","PFs1","PGs1")
+pos_mdl_strs[[4]] <- c("ReB","UnB","ApA","ApC","AnC2","EvB","PGs3")
 
 ##################################################################
 ## Checking Assumptions in each model                           ##
 ##################################################################
 
-#pre_dat <- read_csv("data/PreGuttmanVPL.csv")[,c("UserID","P1s0","P2s0","P3s2","P4s0")]
-#pos_dat <- read_csv("data/PosGuttmanVPL.csv")[,c("UserID","PAs0","PBs0","PCs0","PDs2")]
+pre_dat <- merge(read_csv('data/PreAMC.csv')
+                 , read_csv('data/PreGuttmanVPL.csv'), by = 'UserID', all.x = T)
+pos_dat <- merge(read_csv('data/PosAMC.csv')
+                 , read_csv('data/PosGuttmanVPL.csv'), by = 'UserID', all.x = T)
 
-#verify_mod <- TAM.measure_change.verify(
-#  pre_dat, pos_dat
-#  , items.pre = c("P1s0","P2s0","P3s2","P4s0")
-#  , items.pos = c("PAs0","PBs0","PCs0","PDs2")
-#  , userid = "UserID", irtmodel = "GPCM"
-#  , plotting = T, pairing = T
-#  , folder = "report/learning-outcome/measure-change-model-plots/"
-#)
+verify_mod <- TAM.measure_change.verify(
+  pre_dat, pos_dat
+  , items.pre = pre_mdl_strs[[1]]
+  , items.pos = pos_mdl_strs[[2]]
+  , userid = "UserID", irtmodel = "GPCM"
+  , plotting = T, pairing = T
+  , folder = "report/learning-outcomes/measurement-change-model-plots/"
+)
 
 # print problematic items
-#(problematic_items <- do.call(
-#  rbind,lapply(list(pre.mod = verify_mod$pre_mod
-#                    , pos.mod= verify_mod$pos_mod), FUN = function(mod) {
-#  item_fit <- tam.fit(mod, progress = F)$itemfit
-#  return(item_fit[(item_fit$Outfit > 2 | item_fit$Infit > 2),])
-#})))
+(problematic_items <- do.call(
+  rbind,lapply(list(pre.mod = verify_mod$pre_mod
+                    , pos.mod= verify_mod$pos_mod), FUN = function(mod) {
+  item_fit <- tam.fit(mod, progress = F)$itemfit
+  return(item_fit[(item_fit$Outfit > 2 | item_fit$Infit > 2),])
+})))
 
 ## latex translated
-#tam_mods <- list("Pre-test" = verify_mod$pre_mod, "Post-test" = verify_mod$pos_mod)
-#gpcm_summaries <- get_gpcm_summaries_as_dataframe(tam_mods, estimator = "MLR")
-#filename <- "report/latex/gpcm-learning-outcomes.tex"
-#if (!file.exists(filename)) {
-#  write_gpcm_in_latex(
-#    gpcm_summaries
-#    , in_title = "for measuring gains in the skills and knowledge of participants in the pilot empirical study"
-#    , filename = filename
-#  )
-#}
+tam_mods <- list("Pre-test" = verify_mod$pre_mod, "Post-test" = verify_mod$pos_mod)
+gpcm_summaries <- get_gpcm_summaries_as_dataframe(tam_mods, estimator = "MLR")
+filename <- "report/latex/gpcm-learning-outcomes.tex"
+if (!file.exists(filename)) {
+  write_gpcm_in_latex(
+    gpcm_summaries
+    , in_title = "for measuring gains in the skills and knowledge of participants in the third empirical study"
+    , filename = filename
+  )
+}
 
 ##################################################################
 ## Stacking and Racking                                         ##
 ##################################################################
 
-#mod <- TAM.measure_change(
-#  pre_dat, pos_dat
-#  , items.pre = c("P1s0","P2s0","P3s2","P4s0")
-#  , items.pos = c("PAs0","PBs0","PCs0","PDs2")
-#  , same_items.pre = c("P1s0","P2s0")
-#  , same_items.pos = c("PAs0","PBs0")
-#  , userid = "UserID"
-#  , verify = T, plotting = T, irtmodel = "GPCM")
+mod <- TAM.measure_change(
+  pre_dat, pos_dat
+  , items.pre = pre_mdl_strs[[1]]
+  , items.pos = pos_mdl_strs[[2]]
+  , same_items.pre = c("An3a","An3b","Ev2")
+  , same_items.pos = c("AnC1","AnC2","EvB")
+  , userid = "UserID"
+  , verify = T, plotting = T, irtmodel = "GPCM")
 
-#write_change_measurement_model_plots(
-#  mod, path = 'report/learning-outcome/measure-change-model-plots/', override = T
-#)
+write_change_measurement_model_plots(
+  mod, path = 'report/learning-outcomes/measurement-change-model-plots/', override = T
+)
 
-#write_measure_change_report(
-#  mod, path = 'report/learning-outcome/'
-#  , filename = 'MeasurementChangeModel.xlsx', override = T
-#)
+write_measure_change_report(
+  mod, path = 'report/learning-outcomes/'
+  , filename = 'MeasurementChangeModel.xlsx', override = T
+)
 
-#write_csv(dplyr::mutate(mod$ability, gain.theta = pos.theta-pre.theta), 'data/GainSkillsKnowledge.csv')
+write_csv(dplyr::mutate(mod$ability, gain.theta = pos.theta-pre.theta), 'data/GainSkillsKnowledge.csv')
 
