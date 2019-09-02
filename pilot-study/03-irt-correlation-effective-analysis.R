2daa67d10ec143f157de556c2a7469a5e5844cb6
geiser <651885+geiser@users.noreply.github.com>
2019-09-02 16:15:58 -0300

    Update 03-irt-correlation-effective-analysis.R

--- pilot-study/03-irt-correlation-effective-analysis.R
+++ pilot-study/03-irt-correlation-effective-analysis.R
@@ -4,6 +4,11 @@ library(dplyr)
 library(readxl)
 library(PerformanceAnalytics)
 
+
+source('../common/misc.R')
+source('../common/correlation-analysis.R')
+source('../common/latex-translator.R')
+
 participants <- read_csv('data/EffectiveParticipants.csv')
 
 info_src <- list(
